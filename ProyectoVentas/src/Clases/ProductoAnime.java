5ffda8a63451ba00723592cfbfab6beee806b436
Lluís Muñoz <146749552+lmunoz-dawi@users.noreply.github.com>
2024-04-30 14:40:06 +0200

    Add ProyectoVentas

---
+++ ProyectoVentas/src/Clases/ProductoAnime.java
@@ -0,0 +1,10 @@
+package Clases;
+
+public abstract class ProductoAnime {
+    protected String nombre;
+    protected int precio;
+    protected int stock;
+    protected String descripción;
+    protected String genero;
+    protected String autor;
+}
