c17888121f8b78363292a6253afb41289fe88070
Lluís Muñoz <146749552+lmunoz-dawi@users.noreply.github.com>
2024-04-30 16:10:17 +0200

    Update ProductoAnime.java
    Created Constructor, Getters and Setters

--- ProyectoVentas/src/Clases/ProductoAnime.java
+++ ProyectoVentas/src/Clases/ProductoAnime.java
@@ -4,7 +4,50 @@ public abstract class ProductoAnime {
     protected String nombre;
     protected int precio;
     protected int stock;
-    protected String descripción;
+    protected String descripcion;
     protected String genero;
-    protected String autor;
+
+    public ProductoAnime(String nombre, int precio, int stock, String descripcion, String genero) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+        this.descripcion = descripcion;
+        this.genero = genero;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public int getPrecio(){
+        return precio;
+    }
+    public void setPrecio(int precio){
+        this.precio = precio;
+    }
+
+    public int getStock(){
+        return stock;
+    }
+    public void setStock(int stock){
+        this.stock = stock;
+    }
+    
+    public String getDescripcion(){
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    public String getGenero(){
+        return genero;
+    }
+    public void setGenero(){
+        this.genero = genero;
+    }
 }
