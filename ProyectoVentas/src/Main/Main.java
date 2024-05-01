0bd4da362f8cd5266018d4ace5fd75f63bedd54c
Lluís Muñoz <146749552+lmunoz-dawi@users.noreply.github.com>
2024-05-01 21:46:37 +0200

    Update Main.java

--- ProyectoVentas/src/Main/Main.java
+++ ProyectoVentas/src/Main/Main.java
@@ -4,6 +4,20 @@ import Clases.*;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Figura figura1 = new Figura("Pedro", 3.4, 100, "No hace nada", "Gey");
+        Ropa ropa1 = new Ropa("Sudadera Verde Fosfi",5.0, 50, "Sin limpiar", "Guarro", "XL");
+        Manga manga1 = new Manga("Pedro el sudoroso", 1.2, 100, "Huele fatal enserio", "Roñoso");
+
+        Carrito carrito = new Carrito();
+
+        carrito.AgregarProducto(figura1);
+        carrito.AgregarProducto(ropa1);
+        carrito.AgregarProducto(manga1);
+
+        carrito.MostrarCarrito();
+        carrito.MostrarPrecio();
+
+        System.out.println(figura1.getStock());
+
     }
 }
