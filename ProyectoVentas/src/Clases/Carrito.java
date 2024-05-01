a96dc54d32248cd6c907e9e62760ab1a81e08896
Lluís Muñoz <146749552+lmunoz-dawi@users.noreply.github.com>
2024-05-01 21:49:13 +0200

    Add files via upload

---
+++ ProyectoVentas/src/Clases/Carrito.java
@@ -0,0 +1,36 @@
+package Clases;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Carrito sera una Lista de Objetos
+public class Carrito{
+    public List<Producto> productos ;
+
+    public Carrito(){
+        this.productos = new ArrayList<>();
+    }
+
+    public void AgregarProducto(Producto producto){
+        productos.add(producto);
+        producto.setStock(producto.stock - 1);
+    }
+    public void QuitarProducto(Producto producto){
+        productos.remove(producto);
+        producto.setStock(producto.stock - 1);
+    }
+    public void MostrarCarrito(){
+        for(Producto producto : productos){
+            System.out.print(producto.nombre + " - ");
+            System.out.println(producto.precio);
+        }
+    }
+
+    public void MostrarPrecio(){
+        Double precioTotal = 0.0;
+        for(Producto producto : productos){
+            precioTotal += producto.precio;
+        }
+        System.out.println(precioTotal);
+    }
+}
