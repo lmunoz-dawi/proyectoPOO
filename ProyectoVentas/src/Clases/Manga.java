fac241aaa18a241ab45f99c039b5f08cdc82c787
Lluís Muñoz <146749552+lmunoz-dawi@users.noreply.github.com>
2024-05-03 17:10:19 +0200

    Update Manga.java

--- ProyectoVentas/src/Clases/Manga.java
+++ ProyectoVentas/src/Clases/Manga.java
@@ -2,7 +2,7 @@ package Clases;
 
 public class Manga extends Producto{
     //Constructor
-    public Manga(String nombre, int precio, int stock, String descripcion, String genero) {
+    public Manga(String nombre, Double precio, int stock, String descripcion, String genero) {
         super(nombre, precio, stock, descripcion, genero);
     }
     //Constructor
