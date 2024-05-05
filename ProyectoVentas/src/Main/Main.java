5a3fd4fc26ec9b9548d52fce45676e4642386b48
Lluís Muñoz <146749552+lmunoz-dawi@users.noreply.github.com>
2024-05-05 21:53:48 +0200

    Update Main.java

--- ProyectoVentas/src/Main/Main.java
+++ ProyectoVentas/src/Main/Main.java
@@ -4,9 +4,9 @@ import Clases.*;
 public class Main {
     public static void main(String[] args) {
 
-        Figura figura1 = new Figura("Isabel", 3.4, 100, "Luchadora", "Grey");
-        Ropa ropa1 = new Ropa("Sudadera Verde Fosfi",5.0, 50, "Comoda", "Guerrero", "XL");
-        Manga manga1 = new Manga("El sabueso de Lava", 1.2, 100, "Quema", "Medieval");
+        Figura figura1 = new Figura("Pedro", 3.4, 100, "No hace nada", "Gey");
+        Ropa ropa1 = new Ropa("Sudadera Verde Fosfi",5.0, 50, "Sin limpiar", "Guarro", "XL");
+        Manga manga1 = new Manga("Pedro el sudoroso", 1.2, 100, "Huele fatal enserio", "Roñoso");
 
         Carrito carrito = new Carrito();
 
