be713fd23d9d12b2af7d3aa0b7d90aa0d2ebc541
Lluís Muñoz <146749552+lmunoz-dawi@users.noreply.github.com>
2024-05-22 02:28:31 +0200

    Update Main.java

--- Main/Main.java
+++ Main/Main.java
@@ -10,12 +10,12 @@ public class Main {
 
         Carrito carrito = new Carrito();
 
-        Figura figura1 = new Figura("Melodias", 3.4, 100, "Cazador de Dragones", "Figura");
-        Figura figura2 = new Figura("Naruto", 10.0, 100, "Lider de la banda", "Figura");
-        Ropa ropa1 = new Ropa("Bandana",5.0, 50, "Bandana del caracteristico personaje Naruto", "Ropa", "XL");
-        Ropa ropa2 = new Ropa("Capa",7.0, 50, "Capa Saitama", "Ropa", "Unica");
-        Manga manga1 = new Manga("Berserk", 2.0, 100, "Tragica historia de un luchador", "Manga");
-        Manga manga2 = new Manga("Blue Lock", 2.2, 100, "Equipo de futbol de egoistas", "Manga");
+        Figura figura1 = new Figura("Melodias", 3.4, 100, "Cazador de Dragones", "Figura", "plastico", 432.4 , "Perico");
+        Figura figura2 = new Figura("Naruto", 10.0, 100, "Lider de la banda", "Figura", "Silicona", 10.0, "Ford");
+        Ropa ropa1 = new Ropa("Bandana",5.0, 50, "Bandana del caracteristico personaje Naruto", "Ropa", "XL", "Gris", "Aluminio");
+        Ropa ropa2 = new Ropa("Capa",7.0, 50, "Capa Saitama", "Ropa", "Unica", "Rojo", "Polyester");
+        Manga manga1 = new Manga("Berserk", 2.0, 100, "Tragica historia de un luchador", "Manga", 132, "Martin", "El pato");
+        Manga manga2 = new Manga("Blue Lock", 2.2, 100, "Equipo de futbol de egoistas", "Manga", 543, "Carlos", "El pato");
 
         //Añadimos todos los productos creados al array productos para luego verlo
         productosLista.add(figura1);
@@ -71,7 +71,23 @@ public class Main {
                     carrito.MostrarCarrito();
                     System.out.println(" ");
                     System.out.println("Total del carrito:");
-                    carrito.MostrarPrecio();
+                    System.out.println(carrito.MostrarPrecio() + "€");
+                    // Solicitar cupón de descuento
+                    if(carrito.MostrarPrecio() > 0.0){
+                        System.out.print("Introduce un cupón de descuento (si tienes uno): ");
+                        Scanner sc2 = new Scanner(System.in);
+                        String cupon = sc2.nextLine();
+                        double descuentoAplicado = aplicarDescuento(cupon, carrito.MostrarPrecio());
+                        if (descuentoAplicado > 0) {
+                            System.out.println("¡Cupón válido! Se aplicó un descuento de " + (descuentoAplicado / carrito.MostrarPrecio() * 100) + "%.");
+                            carrito.setPrecioTotal(carrito.MostrarPrecio() - descuentoAplicado);
+                        } else {
+                            System.out.println("Cupón no válido.");
+                        }
+
+                        System.out.println("Total con descuento: " + (carrito.MostrarPrecio()-aplicarDescuento(cupon, carrito.MostrarPrecio())) + "€");
+                        break;
+                    }
                     break;
                 case 5:
                     carrito.VaciarCarrito();
@@ -90,6 +106,16 @@ public class Main {
         for(int i = 0; i < productosLista.size(); i++){
             Producto p = (Producto) productosLista.get(i);
             System.out.println(i + ". " + p.getNombre() + " - Genero: " + p.getGenero());
+        }
+    }
+    public static double aplicarDescuento(String cupon, double precioTotal) {
+        switch (cupon.toUpperCase()) {
+            case "DESCUENTO10":
+                return precioTotal * 0.1;
+            case "MITAD":
+                return precioTotal / 2;
+            default:
+                return 0;
         }
     }
 }
