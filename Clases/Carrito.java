40551375df93f4c28ccd976ce843bebb80c767c1
Lluís Muñoz <146749552+lmunoz-dawi@users.noreply.github.com>
2024-05-22 02:26:55 +0200

    Update Carrito.java

--- Clases/Carrito.java
+++ Clases/Carrito.java
@@ -6,9 +6,22 @@ import java.util.List;
 //Carrito sera una Lista de Objetos
 public class Carrito{
     public List<Producto> productos ;
-
+    private double precioTotal;
     public Carrito(){
         this.productos = new ArrayList<>();
+    }
+
+
+    public double MostrarPrecio(){
+        double precioTotal = 0.0;
+        for(Producto producto : productos){
+            precioTotal += producto.precio;
+        }
+        return precioTotal;
+    }
+
+    public void setPrecioTotal(double nuevoPrecioTotal){
+        this.precioTotal = nuevoPrecioTotal;
     }
 
     public void AgregarProducto(Producto producto){
@@ -19,21 +32,14 @@ public class Carrito{
         productos.remove(producto);
         producto.setStock(producto.stock - 1);
     }
+
     public void MostrarCarrito(){
         for(Producto producto : productos){
-            System.out.print(producto.nombre + " - ");
-            System.out.print(producto.precio + "€ ");
-            System.out.println(producto.descripcion);
+            System.out.print(producto.mostrarDetalles());
         }
     }
+
     public void VaciarCarrito(){
         productos.clear();
-    }
-    public void MostrarPrecio(){
-        Double precioTotal = 0.0;
-        for(Producto producto : productos){
-            precioTotal += producto.precio;
-        }
-        System.out.println(precioTotal + "€");
     }
 }
