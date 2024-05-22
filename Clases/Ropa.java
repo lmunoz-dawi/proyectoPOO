81cee4640e7e5363524847908294ec1a4bf31b43
Lluís Muñoz <146749552+lmunoz-dawi@users.noreply.github.com>
2024-05-22 02:27:46 +0200

    Update Ropa.java

--- Clases/Ropa.java
+++ Clases/Ropa.java
@@ -1,23 +1,45 @@
 package Clases;
 
-public class Ropa extends Producto{
-    public String tallas;
+public class Ropa extends Producto implements Detalles {
+    private String talla;
+    private String color;
+    private String material;
 
     //Constructor
-    public Ropa(String nombre, Double precio, int stock, String descripcion,String genero, String tallas) {
+    public Ropa(String nombre, Double precio, int stock, String descripcion,String genero, String talla, String color, String material) {
         super(nombre, precio, stock, descripcion, genero);
-        this.tallas = tallas;
+        this.talla = talla;
+        this.color = color;
+        this.material = material;
 
     }
-    public String getTallas() {
-        return tallas;
+    public String getTalla() {
+        return talla;
     }
-    public void setTallas(String tallas) {
-        this.tallas = tallas;
-    }
-
-    public double getPrecioConDescuento(){
-        return Producto.aplicarDescuento(getPrecio());
+    public void setTalla(String talla) {
+        this.talla = talla;
     }
 
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+
+    @Override
+    public String mostrarDetalles() {
+        return String.format("Ropa: %s\s- Talla: %s\s- Color: %s\s- Material: %s\s- Precio: %.2f€ \n",
+                this.getNombre(), this.getTalla(), this.getColor(), this.getMaterial(), this.getPrecio());
+    }
 }
